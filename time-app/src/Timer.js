c450cedaa76041f78db7db96ea9731d260313906
Jongsoo Yoon <jongsoojong@gmail.com>
2017-10-24 19:37:51 -0700

    Added Dope Ass Timer

---
+++ time-app/src/Timer.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { secondsElapsed: 0 }
+    }
+
+    tick() {
+        this.setState((prevState) => ({
+            secondsElapsed : prevState.secondsElapsed + 1
+        }));
+    }
+
+    timeFormat(time) {
+        let hours = Math.floor(time/3600);
+        let minutes = Math.floor((time % 3600) / 60);
+        let seconds = time % 60;
+
+        let results = '';
+
+        if(hours > 0) {
+            results += '' + hours + ':' + (minutes < 1 ? '0' : '');
+        }
+
+        results += '' + minutes + ':' + (seconds < 10 ? '0' : '');
+        results += '' + seconds;
+        return results
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        return (
+            <div> 
+                <h2> Jong's Dope Ass Timer </h2>
+                <p> {this.timeFormat(this.state.secondsElapsed)} </p>
+            </div>
+        )
+    }
+
+}
+
+export default Timer;
+
+
